cabae347dcc21f01db9b1805bd7a7c4d36525435
sjswan13 <sjswan13@gmail.com>
2024-08-08 15:59:59 -0600

    initial JS

---
+++ script.js
@@ -0,0 +1,50 @@
+const search = document.getElementById('search'),
+  submit=document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('results-heading'),
+  single_mealEl = document.getElementById('single-meal');
+
+//Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  //Clear Single meal
+  single_mealEl.innerHTML='';
+
+  //Get search term
+  const term = search.value;
+
+  //Check for empty
+  if(term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+      if(data.meals === null) {
+        resultHeading.innerHTML = `<p>There are no search results for '${term}'. Try Again!</p>`
+      } else {
+        mealsEl.innerHTML = data.meals.map(meal => `
+          <div class='meal'>
+            <img src='${meal.strMealThumb}' alt='${meal.str}'/>
+            <div class='meal-info' data-mealID='${meal.idMeal}'>
+              <h3>${meal.strMeal}</h3>
+            </div>
+          </div>
+        `
+          )
+          .join('');
+      }
+      //Clear Search Text
+      search.value= '';
+    });
+  }else {
+    alert('Please enter a search term');
+  }
+}
+
+
+//Event Listeners
+  submit.addEventListener('submit', searchMeal);
